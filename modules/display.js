e21fc3a3039b9fb50570ea85c098bfeff3d4c972
Atlas2234 <zepro6966@gmail.com>
2024-05-03 16:44:41 -0400

    added functionality to check and ensure the same quote does not show up twice when generating the next one

--- modules/display.js
+++ modules/display.js
@@ -2,12 +2,28 @@ import randomNum from './randomNum.js'
 // Display the quotes from the fetched data
 const displayQuotes = (data, container) => {
  // Remove the loading animation
-  container.classList.remove('loading')
+  container.classList.remove('loading');
 
   // Data is an array of objects containing the quote and relevant information about the quote
 
   // Randomly generate a number
-  const rando = randomNum();
+  let rando = randomNum();
+
+  // Access session storage
+  const seshStore = sessionStorage.getItem('rand');
+
+  // If the number was previously generated then pick a different number
+  if(seshStore == null) {
+   // For the first time this page is opened
+   sessionStorage.setItem('rand', rando);
+  } else if (rando == seshStore) {
+   // When the random number generated is the same as the previous one that is stored
+   // Keep generating another number until they are different
+   while (rando == seshStore) {
+    rando = randomNum();
+   }
+  }
+  sessionStorage.setItem('rand', rando);
 
   // Access the randomly generated number's index in the data array and get the quote from that array item
   const inspiration = data[rando].text;
